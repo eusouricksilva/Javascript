68bf82ed1e4e09f65a1f6aca900b7052afed85ab
Pedro Henrique <pedrohenriquedasilvacontato@gmail.com>
2023-04-13 17:07:50 -0300

    up

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 30
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) { //Condição Simples!
+    console.log('Você ultrapassou a velocidade. MULTADO!')
+}
+console.log('Dirija smepre usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Brasil'
+console.log(`DIga-me seu País: ${país
+} `)
+if ( país == 'Brasil') {
+    console.log ('Você é Brasileiro!')
+}
+else {
+    console.log ('Você é estrangeiro!')
+}
